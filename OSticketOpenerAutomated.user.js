075708250e1ee090762e17eb10f212af4c125f07
Teuzer <49661016+Teuzer@users.noreply.github.com>
2021-01-17 19:00:14 -0300

    Create OSticketOpenerAutomated.user.js

---
+++ OSticketOpenerAutomated.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Automatic OSticket Opener
+// @author       Matheus Augusto de Jesus Madeira
+// @version      6
+// @downloadURL  https://github.com/Teuzer/OSticket-Automation/raw/master/OSticketOpenerAutomated.user.js
+// @updateURL    https://github.com/Teuzer/OSticket-Automation/raw/master/OSticketOpenerAutomated.user.js
+// @description  Design to automate OSticket's open application.
+// @require      https://cdn.jsdelivr.net/npm/notiflix@2.4.0/dist/notiflix-aio-2.4.0.min.js
+// @require      https://raw.githubusercontent.com/SheetJS/sheetjs/master/dist/xlsx.full.min.js
+// @require      https://raw.githubusercontent.com/eligrey/FileSaver.js/master/dist/FileSaver.min.js
+// @match        http://10.154.77.34/osticket/open.php*
+// @match        http://10.154.77.34/osticket/tickets.php?id=*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+(function(){})();
